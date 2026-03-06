49765a41b68613a08ea2e00096b5d3d5eda0318b
unknown <kurtlacro15@gmail.com>
2026-03-07 00:53:47 +0800

    chore: increase chunk size limit in vite config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 5000, // Raises limit to 5MB so the warning disappears
+  },
 })
